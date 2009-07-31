2d6ed7f860e0e6d4b8685537ded71401478e2783
joseph <joseph@dreamoval.com>
2009-07-31 17:12:20 +0000

    Implemented Generic DAO strategy for dao, created dao factory , added spring configuration to the project.

--- motech-mobile-core/src/main/java/com/dreamoval/motech/core/dao/hibernate/testSession.java
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/core/dao/hibernate/testSession.java
@@ -4,6 +4,11 @@
  */
 
 package com.dreamoval.motech.core.dao.hibernate;
+import com.dreamoval.motech.core.model.MessageDetails;
+import java.util.Date;
+import org.hibernate.Session;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *  Date : Jul 31, 2009
@@ -13,7 +18,15 @@ package com.dreamoval.motech.core.dao.hibernate;
 public class testSession {
 
     public static void main(String[] args){
-        
+        MessageDetails m = new MessageDetails("text",2,"test for dummies 5","233244567890,23322334455","Failed",new Date());
+        ApplicationContext act = new ClassPathXmlApplicationContext("motech-core.xml");
+        HibernateUtils sessionManager = (HibernateUtils)act.getBean("sessionManager");
+        Session session = sessionManager.requestSession();
+        session.beginTransaction();
+        session.save(m);
+        session.getTransaction().commit();
+
+
     }
 
 }
