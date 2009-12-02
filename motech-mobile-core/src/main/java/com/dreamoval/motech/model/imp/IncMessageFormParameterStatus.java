b7e4d618897bc8a0074bc247f46dc6a0831815cc
joseph <joseph@dreamoval.com>
2009-12-02 21:18:35 +0000

    Added IncMessageFormParameterStatus for IncomingMessageFormParameterImpl class and added comment on the status

---
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/imp/IncMessageFormParameterStatus.java
@@ -0,0 +1,13 @@
+package com.dreamoval.motech.model.imp;
+
+/*
+ * IncMessageFormParameterStatus is an enumeration that describes the state of an instance of IncomingMessageFormParameterImpl object
+ * Status can be Started or ended or stopped
+ *
+ * Date: Dec 02, 2009
+ * @author Joseph Djomeda (joseph@dreamoval.com)
+ */
+public enum IncMessageFormParameterStatus {
+
+    NEW, VALID, INVALID;
+}
