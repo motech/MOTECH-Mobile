c962db94cdb810b1546e5db7d221a004efe98832
joseph <joseph@dreamoval.com>
2009-12-02 20:19:19 +0000

    Added IncMessageStatus for IncomingMessageImpl class

---
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/dao/imp/IncMessageStatus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dreamoval.motech.model.dao.imp;
+
+/**
+ *
+ * @author jojo
+ */
+public enum IncMessageStatus {
+    PROCESSING, PROCESSED;
+
+}
