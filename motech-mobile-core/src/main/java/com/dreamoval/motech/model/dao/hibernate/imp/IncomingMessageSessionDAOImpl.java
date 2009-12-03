18dc3e0eb0dfb99a6214b22f64b5bd27b88af868
joseph <joseph@dreamoval.com>
2009-12-03 20:18:29 +0000

    Added IncomingMessageSessionDAO and IncomingMessageSessionDAOImpl and their entry in core-config.xml and test-core-config.xml

---
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/dao/hibernate/imp/IncomingMessageSessionDAOImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dreamoval.motech.model.dao.hibernate.imp;
+
+import com.dreamoval.motech.core.dao.hibernate.HibernateGenericDAOImpl;
+import com.dreamoval.motech.model.dao.imp.IncomingMessageSessionDAO;
+import com.dreamoval.motech.model.imp.IncomingMessageSessionImpl;
+
+/**
+ *
+ * @author jojo
+ */
+public class IncomingMessageSessionDAOImpl extends HibernateGenericDAOImpl<IncomingMessageSessionImpl> implements IncomingMessageSessionDAO<IncomingMessageSessionImpl> {
+
+}
---
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/dao/imp/IncomingMessageSessionDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dreamoval.motech.model.dao.imp;
+
+import com.dreamoval.motech.core.dao.GenericDAO;
+import com.dreamoval.motech.model.imp.IncomingMessageSession;
+
+/**
+ *
+ * @author jojo
+ */
+public interface IncomingMessageSessionDAO<T extends IncomingMessageSession> extends GenericDAO<T>{
+
+}
