2bb966331ab6f9fc9c7c80ebe7dd502eca912eaa
joseph <joseph@dreamoval.com>
2009-12-03 14:48:43 +0000

    corrected method access type

--- motech-mobile-core/src/main/java/com/dreamoval/motech/model/imp/IncomingMessageForm.java
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/imp/IncomingMessageForm.java
@@ -3,56 +3,55 @@ package com.dreamoval.motech.model.imp;
 import java.util.Date;
 import java.util.Set;
 
-
 public interface IncomingMessageForm {
 
     /**
      * @return the dateCreated
      */
-    Date getDateCreated();
+    public Date getDateCreated();
 
     /**
      * @return the incomingMsgFormDefinition
      */
-    IncomingMessageFormDefinition getIncomingMsgFormDefinition();
+    public IncomingMessageFormDefinition getIncomingMsgFormDefinition();
 
     /**
      * @return the lastModified
      */
-    Date getLastModified();
+    public Date getLastModified();
 
     /**
      * @return the messageStatus
      */
-    IncMessageFormStatus getMessageStatus();
+    public IncMessageFormStatus getMessageStatus();
 
     /**
      * @return the incomingMsgFormParameters
      */
-    Set<IncomingMessageFormParameter> getIncomingMsgFormParameters( );
+    public Set<IncomingMessageFormParameter> getIncomingMsgFormParameters();
 
     /**
      * @param dateCreated the dateCreated to set
      */
-    void setDateCreated(Date dateCreated);
+    public void setDateCreated(Date dateCreated);
 
     /**
      * @param incomingMsgFormDefinition the incomingMsgFormDefinition to set
      */
-    void setIncomingMsgFormDefinition(IncomingMessageFormDefinition incomingMsgFormDefinition);
+    public void setIncomingMsgFormDefinition(IncomingMessageFormDefinition incomingMsgFormDefinition);
 
     /**
      * @param lastModified the lastModified to set
      */
-    void setLastModified(Date lastModified);
+    public void setLastModified(Date lastModified);
 
     /**
      * @param messageStatus the messageStatus to set
      */
-    void setMessageStatus(IncMessageFormStatus messageStatus);
+    public void setMessageStatus(IncMessageFormStatus messageStatus);
 
     /**
      * @param incomingMsgFormParameters the incomingMsgFormParameters to set
      */
-    void setIncomingMsgFormParameters(Set<IncomingMessageFormParameter> incomingMsgFormParameters);
+    public void setIncomingMsgFormParameters(Set<IncomingMessageFormParameter> incomingMsgFormParameters);
 }
