41c0c321e52ef2c3b51a4d18c59a2ff367fa4d70
joseph <joseph@dreamoval.com>
2009-12-03 14:47:33 +0000

    corrected method access type

--- motech-mobile-core/src/main/java/com/dreamoval/motech/model/imp/IncomingMessageFormDefinition.java
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/model/imp/IncomingMessageFormDefinition.java
@@ -12,40 +12,40 @@ public interface IncomingMessageFormDefinition {
     /**
      * @return the dateCreated
      */
-    Date getDateCreated();
+    public Date getDateCreated();
 
     /**
      * @return the form_code
      */
-    String getForm_code();
+    public String getForm_code();
 
     /**
      * @return the lastModified
      */
-    Date getLastModified();
+    public Date getLastModified();
 
     /**
      * @return the incomingMsgParamDefinition
      */
-    Set<IncomingMessageFormParameterDefinition> getIncomingMsgParamDefinition( );
+    public Set<IncomingMessageFormParameterDefinition> getIncomingMsgParamDefinition();
 
     /**
      * @param dateCreated the dateCreated to set
      */
-    void setDateCreated(Date dateCreated);
+    public void setDateCreated(Date dateCreated);
 
     /**
      * @param form_code the form_code to set
      */
-    void setForm_code(String form_code);
+    public void setForm_code(String form_code);
 
     /**
      * @param lastModified the lastModified to set
      */
-    void setLastModified(Date lastModified);
+    public void setLastModified(Date lastModified);
 
     /**
      * @param incomingMsgParamDefinition the incomingMsgParamDefinition to set
      */
-    void setIncomingMsgParamDefinition(Set<IncomingMessageFormParameterDefinition> incomingMsgParamDefinition);
+    public void setIncomingMsgParamDefinition(Set<IncomingMessageFormParameterDefinition> incomingMsgParamDefinition);
 }
