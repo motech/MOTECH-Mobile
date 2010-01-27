f128c83e2be9cf88766a682f81becef3c097d052
joseph <joseph@dreamoval.com>
2010-01-27 18:00:09 +0000

    Simple clean up of core.service

--- motech-mobile-core/src/main/java/com/dreamoval/motech/core/service/MotechContext.java
+++ motech-mobile-core/src/main/java/com/dreamoval/motech/core/service/MotechContext.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.dreamoval.motech.core.service;
 
 import com.dreamoval.motech.core.dao.DBSession;
@@ -17,7 +12,7 @@ import java.io.Serializable;
  * @author Henry Sampson
  * Date Created 31-07-2009
  */
-public interface MotechContext<S, TX> extends Serializable{
+public interface MotechContext<S, TX> extends Serializable {
 
     /**
      * Sets the DB Session for the context
