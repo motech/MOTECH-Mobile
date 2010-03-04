8c570c3bdcadf18006135a4620c267e59a94b77a
k.asamoah <k.asamoah@dreamoval.com>
2010-03-04 18:36:36 +0000

    Added registrar-services.xml config file Added MethodSignature class

---
+++ motech-mobile-imp/src/main/java/com/dreamoval/motech/imp/util/MethodSignature.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dreamoval.motech.imp.util;
+
+import java.util.Map;
+
+/**
+ *
+ * @author user
+ */
+public class MethodSignature {
+    private String methodName;
+    private Map<String, Class> methodParams;
+    private Class returnType = null;
+
+    /**
+     * @return the methodName
+     */
+    public String getMethodName() {
+        return methodName;
+    }
+
+    /**
+     * @param methodName the methodName to set
+     */
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    /**
+     * @return the returnType
+     */
+    public Class getReturnType() {
+        return returnType;
+    }
+
+    /**
+     * @param returnType the returnType to set
+     */
+    public void setReturnType(Class returnType) {
+        this.returnType = returnType;
+    }
+
+    /**
+     * @return the methodParams
+     */
+    public Map<String, Class> getMethodParams() {
+        return methodParams;
+    }
+
+    /**
+     * @param methodParams the methodParams to set
+     */
+    public void setMethodParams(Map<String, Class> methodParams) {
+        this.methodParams = methodParams;
+    }
+}
