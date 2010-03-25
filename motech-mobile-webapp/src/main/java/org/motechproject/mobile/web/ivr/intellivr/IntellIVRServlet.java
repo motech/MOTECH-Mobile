23ce6eee95c707147365bc4439df6588d2af8074
fcbrooks <fcbrooks@usm.maine.edu>
2010-03-25 20:17:36 +0000

    Added new servlet for interaction with INTELLIVR system

---
+++ motech-mobile-webapp/src/main/java/org/motechproject/mobile/web/ivr/intellivr/IntellIVRServlet.java
@@ -0,0 +1,28 @@
+package org.motechproject.mobile.web.ivr.intellivr;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IntellIVRServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/xml");
+		PrintWriter out = resp.getWriter();
+		out.println("<test>testing</test>");
+		
+	}
+
+}
