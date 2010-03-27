cd234268aae188979378ae62eef48bb7991ee6a7
fcbrooks <fcbrooks@usm.maine.edu>
2010-03-27 19:42:18 +0000

    ReportHandler - changed return type of handler method

--- motech-mobile-omp/src/main/java/org/motechproject/mobile/omp/manager/intellivr/ReportHandler.java
+++ motech-mobile-omp/src/main/java/org/motechproject/mobile/omp/manager/intellivr/ReportHandler.java
@@ -1,9 +1,10 @@
 package org.motechproject.mobile.omp.manager.intellivr;
 
 import org.motechproject.omp.manager.intellivr.ReportType;
+import org.motechproject.omp.manager.intellivr.ResponseType;
 
 public interface ReportHandler {
 	
-	void handleReport(ReportType report);
+	ResponseType handleReport(ReportType report);
 	
 }
