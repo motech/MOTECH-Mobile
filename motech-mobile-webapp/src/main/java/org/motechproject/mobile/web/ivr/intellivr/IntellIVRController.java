09db31bfbcb57b52e2cd7346b1b913a9d62d65ab
fcbrooks <fcbrooks@usm.maine.edu>
2010-03-29 18:35:57 +0000

    IntellIVRController.java - updated to validate incoming XML content against schema.  Return simple test error/non-error output. Added an init method and wire it via the intellivr-servlet.xml config file.

--- motech-mobile-webapp/src/main/java/org/motechproject/mobile/web/ivr/intellivr/IntellIVRController.java
+++ motech-mobile-webapp/src/main/java/org/motechproject/mobile/web/ivr/intellivr/IntellIVRController.java
@@ -1,5 +1,11 @@
 package org.motechproject.mobile.web.ivr.intellivr;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,45 +14,108 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import org.motechproject.omp.manager.intellivr.AutoCreate;
 import org.motechproject.omp.manager.intellivr.ErrorCodeType;
 import org.motechproject.omp.manager.intellivr.ResponseType;
 import org.motechproject.omp.manager.intellivr.StatusType;
+import org.motechproject.omp.manager.intellivr.RequestType.Vxml;
 
-public class IntellIVRController extends AbstractController {
+public class IntellIVRController extends AbstractController implements ResourceLoaderAware {
 
+	private ResourceLoader resourceLoader;
+	private DocumentBuilder parser;
+	private Validator validator;
+	
+	public void init() {
+		Resource schemaResource = resourceLoader.getResource("classpath:intellivr-in.xsd");
+		SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+		try {
+			Schema schema = factory.newSchema(schemaResource.getFile());
+			parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			validator = schema.newValidator();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FactoryConfigurationError e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
-		InputStream in = request.getInputStream();
-		int length = request.getContentLength();
-		byte[] buffer = new byte[length];
-		int read = in.read(buffer, 0, length);
+		String content = getContent(request);
 		
-		String content = new String(buffer);
+		Object output = null;
 		
-		response.setContentType("text/xml");
+		if ( !contentIsValid(content) ) {
+			AutoCreate ac = new AutoCreate();
+			ResponseType rt = new ResponseType();
+			rt.setStatus(StatusType.ERROR);
+			rt.setErrorCode(ErrorCodeType.MOTECH_MALFORMED_XML);
+			rt.setErrorString("Malformed XML content");
+			ac.setResponse(rt);
+			output = ac;
+		} else {
+			AutoCreate ac = new AutoCreate();
+			ResponseType rt = new ResponseType();
+			rt.setStatus(StatusType.OK);
+			rt.setLanguage("ENGLISH");
+			rt.setPrivate("PRIVATE");
+			rt.setReportUrl("http://130.111.123.83:8080/motech-mobile-webapp/intellivr");
+			rt.setTree("TestTree");
+			Vxml vxml = new Vxml();
+			rt.setVxml(vxml);
+			ac.setResponse(rt);
+			output = ac;
+		}
+
+		if ( output == null ) {
+			AutoCreate ac = new AutoCreate();
+			ResponseType rt = new ResponseType();
+			rt.setStatus(StatusType.ERROR);
+			rt.setErrorCode(ErrorCodeType.MOTECH_UNKNOWN_ERROR);
+			rt.setErrorString("An unknown error has occured");
+			ac.setResponse(rt);
+			output = ac;
+		}
+		
 		PrintWriter out = response.getWriter();
 		
-		AutoCreate ac = new AutoCreate();
-		ResponseType rt = new ResponseType();
-		rt.setStatus(StatusType.ERROR);
-		rt.setErrorCode(ErrorCodeType.MOTECH_MALFORMED_XML);
-		rt.setErrorString("");
-		ac.setResponse(rt);
-	
 		try {
+			response.setContentType("text/xml");
 			JAXBContext jac = JAXBContext.newInstance("org.motechproject.omp.manager.intellivr");
 			Marshaller m = jac.createMarshaller();
-			m.marshal(ac, out);
+			m.marshal(output, out);
 		} catch (JAXBException e) {
 			e.printStackTrace();
 			out.println("<test>jaxb error</test>");
@@ -56,4 +125,32 @@ public class IntellIVRController extends AbstractController {
 		return null;
 	}
 
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
+	private String getContent(HttpServletRequest request) throws Exception {
+		InputStream in = request.getInputStream();
+		int length = request.getContentLength();
+		byte[] buffer = new byte[length];
+		in.read(buffer, 0, length);
+		return new String(buffer);
+	}
+	
+	private boolean contentIsValid(String content) {
+		
+		try {
+			Document doc = parser.parse(new ByteArrayInputStream(content.getBytes()));
+			validator.validate(new DOMSource(doc));
+			return true;
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
 }
