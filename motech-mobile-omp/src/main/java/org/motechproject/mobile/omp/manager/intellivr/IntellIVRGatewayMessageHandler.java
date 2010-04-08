740c8839f895991657c8d14f344f6d24a9649437
fcbrooks <fcbrooks@usm.maine.edu>
2010-04-08 18:33:11 +0000

    Started the implementation of GatewayMessageHandler for IntellIVR GatewayManager.  Just added basic structure for injecting status maps and returning values from them.

--- motech-mobile-omp/src/main/java/org/motechproject/mobile/omp/manager/intellivr/IntellIVRGatewayMessageHandler.java
+++ motech-mobile-omp/src/main/java/org/motechproject/mobile/omp/manager/intellivr/IntellIVRGatewayMessageHandler.java
@@ -1,5 +1,6 @@
 package org.motechproject.mobile.omp.manager.intellivr;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.motechproject.mobile.core.manager.CoreManager;
@@ -11,20 +12,11 @@ import org.motechproject.mobile.omp.manager.GatewayMessageHandler;
 
 public class IntellIVRGatewayMessageHandler implements GatewayMessageHandler {
 
-	public CoreManager getCoreManager() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public MStatus lookupResponse(String code) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public MStatus lookupStatus(String code) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	MStatus defaultStatus = MStatus.PENDING;
+	MStatus defaultResponse = MStatus.SCHEDULED;
+	private Map<String, MStatus> statusMap;
+	private Map<String, MStatus> responseMap;
+	private CoreManager coreManager;
 	
 	@SuppressWarnings("unchecked")
 	public Set<GatewayResponse> parseMessageResponse(GatewayRequest message,
@@ -34,13 +26,41 @@ public class IntellIVRGatewayMessageHandler implements GatewayMessageHandler {
 	}
 
 	public MStatus parseMessageStatus(String messageStatus) {
-		// TODO Auto-generated method stub
-		return null;
+		return lookupStatus(messageStatus);
+	}
+
+	public MStatus lookupResponse(String code) {
+		MStatus responseStatus = responseMap.get(code);
+		return responseStatus != null ? responseStatus : defaultResponse;
+	}
+
+	public MStatus lookupStatus(String code) {
+		MStatus status = responseMap.get(code);
+		return status != null ? status : defaultStatus;
+	}
+	
+	public CoreManager getCoreManager() {
+		return coreManager;
 	}
 
 	public void setCoreManager(CoreManager coreManager) {
-		// TODO Auto-generated method stub
+		this.coreManager = coreManager;
+	}
+	
+	public Map<String, MStatus> getStatusMap() {
+		return statusMap;
+	}
 
+	public void setStatusMap(Map<String, MStatus> statusMap) {
+		this.statusMap = statusMap;
+	}
+
+	public Map<String, MStatus> getResponseMap() {
+		return responseMap;
+	}
+
+	public void setResponseMap(Map<String, MStatus> responseMap) {
+		this.responseMap = responseMap;
 	}
 
 }
